246bed12b4d2a4c68f7d9fc5f0aa87928b88def7
Andrés Alfaya Mariño <andres.alfaya@ctag.com>
2025-11-13 12:42:58 +0100

    Fix cicd

--- terraform/deployment.tf
+++ terraform/deployment.tf
@@ -1,35 +1,35 @@
 resource "kubernetes_manifest" "backend_deployment" {
-  manifest = yamldecode(templatefile("${path.module}/../k8s/${var.env}/backend-deployment.yaml", {
+  manifest = yamldecode(templatefile("${path.module}/../k8s/backend-deployment.yaml", {
     backend_image = var.backend_image
   }))
 }
 
 resource "kubernetes_manifest" "backend_service" {
-  manifest = yamldecode(templatefile("${path.module}/../k8s/${var.env}/backend-service.yaml", {
+  manifest = yamldecode(templatefile("${path.module}/../k8s/backend-service.yaml", {
     env              = var.env,
     backend_nodeport = var.ports[var.env].backend_nodeport
   }))
 }
 
 resource "kubernetes_manifest" "frontend_deployment" {
-  manifest = yamldecode(templatefile("${path.module}/../k8s/${var.env}/frontend-deployment.yaml", {
+  manifest = yamldecode(templatefile("${path.module}/../k8s/frontend-deployment.yaml", {
     frontend_image = var.frontend_image
   }))
 }
 
 resource "kubernetes_manifest" "backend_service" {
-  manifest = yamldecode(templatefile("${path.module}/../k8s/${var.env}/backend-service.yaml", {
+  manifest = yamldecode(templatefile("${path.module}/../k8s/backend-service.yaml", {
     env              = var.env,
     backend_nodeport = var.ports[var.env].backend_nodeport
   }))
 }
 
 resource "kubernetes_manifest" "postgres_statefulset" {
-  manifest = yamldecode(file("${path.module}/../k8s/${var.env}/postgres-statefulset.yaml"))
+  manifest = yamldecode(file("${path.module}/../k8s/postgres-statefulset.yaml"))
 }
 
 resource "kubernetes_manifest" "postgres_service" {
-  manifest = yamldecode(templatefile("${path.module}/../k8s/${var.env}/postgres-service.yaml", {
+  manifest = yamldecode(templatefile("${path.module}/../k8s/postgres-service.yaml", {
     env = var.env
   }))
 }
