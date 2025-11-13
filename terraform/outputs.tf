6d311582d1eac30cf5ec89c4378a3690dae3c708
Andrés Alfaya Mariño <andres.alfaya@ctag.com>
2025-11-13 18:56:59 +0100

    Fic

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,19 +1,7 @@
-output "frontend_url" {
-  description = "Frontend accesible en localhost"
-  value       = "http://localhost:${var.ports[var.env].frontend_nodeport}"
-}
-
-output "frontend_health_url" {
-  description = "Endpoint de health del frontend en localhost"
-  value       = "http://localhost:${var.ports[var.env].frontend_nodeport}/health"
-}
-
 output "backend_url" {
-  description = "Backend accesible en localhost"
-  value       = "http://localhost:${var.ports[var.env].backend_nodeport}"
+  value = "http://localhost:${var.ports[var.env].backend_nodeport}/api/health"
 }
 
-output "backend_health_url" {
-  description = "Endpoint de health del backend en localhost"
-  value       = "http://localhost:${var.ports[var.env].backend_nodeport}/api/health"
+output "frontend_url" {
+  value = "http://localhost:${var.ports[var.env].frontend_nodeport}/health"
 }
