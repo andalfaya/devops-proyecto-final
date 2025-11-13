7537df68c6d88493172179a9220256a618f92c2d
Andrés Alfaya Mariño <andres.alfaya@ctag.com>
2025-11-13 17:48:51 +0100

    Fix cicd

--- terraform/deployment.tf
+++ terraform/deployment.tf
@@ -1,7 +1,7 @@
 resource "kubernetes_manifest" "backend_deployment" {
   manifest = yamldecode(templatefile("${path.module}/../k8s/backend-deployment.yaml", {
-    backend_image = var.backend_image,
-    env           = var.env,
+    backend_image    = var.backend_image,
+    env              = var.env,
     backend_nodeport = var.ports[var.env].backend_nodeport
   }))
 }
@@ -15,8 +15,8 @@ resource "kubernetes_manifest" "backend_service" {
 
 resource "kubernetes_manifest" "frontend_deployment" {
   manifest = yamldecode(templatefile("${path.module}/../k8s/frontend-deployment.yaml", {
-    frontend_image = var.frontend_image,
-    env            = var.env,
+    frontend_image    = var.frontend_image,
+    env               = var.env,
     frontend_nodeport = var.ports[var.env].frontend_nodeport
   }))
 }
